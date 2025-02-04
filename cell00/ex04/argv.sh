f575af25470ef816627bee565e884ab067c1d0f9
Kawin Isaramala <138878357+KwnxpN@users.noreply.github.com>
2025-02-04 21:37:30 +0700

    Update argv.sh

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -7,12 +7,18 @@ else
     count=1
     for arg in "$@"
     do
-        if [ $count -eq $# ]
+        if [ $count -eq 3 ]
         then
             echo -n "$arg"
-        else
-            echo -e "$arg"
+            break
         fi
+        else
+            if [ $count -eq $# ]
+            then
+                echo -n "$arg"
+            else
+                echo -e "$arg"
+            fi
         count=$((count + 1))
     done
 fi
